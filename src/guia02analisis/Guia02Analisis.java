0ce1c0854165a76d1021c908a336a500396b1fde
robal <robal@AREVALO>
2018-03-13 03:58:39 -0600

    aun falta interfas

--- src/guia02analisis/Guia02Analisis.java
+++ src/guia02analisis/Guia02Analisis.java
@@ -1,16 +1,8 @@
-
 package guia02analisis;
-
-
+import Interfaces.GeneralMenu;
 public class Guia02Analisis {
     public static void main(String[] args) {
-        Bisecciones b =new Bisecciones();
-        FalsaPosicion p=new FalsaPosicion();
-        System.out.println(Metodos.Decimal(b.Biseccion5(1,4)));
-        System.out.println(Metodos.Decimal(b.Biseccion5(2,4)));
-        System.out.println("");
-        System.out.println(Metodos.Decimal(p.FalsaPos5(1,4)));
-        System.out.println(Metodos.Decimal(p.FalsaPos5(2,4)));
-        
+        GeneralMenu gm =new GeneralMenu();
+        gm.show();
     }
 }
